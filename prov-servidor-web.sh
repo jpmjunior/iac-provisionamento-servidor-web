77ac2c63600b7d3b842a87e7e02c1892deb346dc
jpmjunior <junior.linhares@yahoo.com.br>
2023-02-19 16:52:57 -0300

    Exibindo comentários com comando echo

--- prov-servidor-web.sh
+++ prov-servidor-web.sh
@@ -3,23 +3,23 @@
 # Este script coloca em produção um um projeto HTML em um servidor
 # Apache instalado no Linux Ubuntu
 
-# atualizando sistema e instalando pacotes
+echo "Atualizando sistema e instalando pacotes..."
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-# removendo pagina inicial do apache
+echo "Removendo pagina inicial do apache..."
 rm /var/www/html/index.html
 
-# baixando projeto e colocando em produção
+echo "Baixando projeto e colocando em produção..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp * /var/www/html -r
 
-# excluindo arquivos temporários
+echo "Excluindo arquivos temporários..."
 cd /tmp
 rm main.zip
 rm -r linux-site-dio-main
