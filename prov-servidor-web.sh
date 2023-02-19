65c082e068464223ff248df48170fecf4084515f
jpmjunior <junior.linhares@yahoo.com.br>
2023-02-19 17:02:42 -0300

    Melhorando visualização do comando echo

--- prov-servidor-web.sh
+++ prov-servidor-web.sh
@@ -3,23 +3,23 @@
 # Este script coloca em produção um um projeto HTML em um servidor
 # Apache instalado no Linux Ubuntu
 
-echo "Atualizando sistema e instalando pacotes..."
+echo -e "###\nAtualizando sistema e instalando pacotes...\n###"
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-echo "Removendo pagina inicial do apache..."
+echo -e "###\nRemovendo pagina inicial do apache...\n###"
 rm /var/www/html/index.html
 
-echo "Baixando projeto e colocando em produção..."
+echo -e "###\nBaixando projeto e colocando em produção...\n###"
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp * /var/www/html -r
 
-echo "Excluindo arquivos temporários..."
+echo -e "###\nExcluindo arquivos temporários...\n###"
 cd /tmp
 rm main.zip
 rm -r linux-site-dio-main
